9e099fbf1e4c80e4f7b4efd0dcdb5cb7a0ef78d0
Luke Xavier Symington <lxsymington@gmail.com>
2020-11-10 22:14:33 +0000

    LSP keymaps

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -1,33 +1,8 @@
+local vimp = require('vimp')
 local nvim_lsp = require('nvim_lsp')
 local lsp_status = require('lsp-status')
 local completion = require('completion')
 local diagnostic = require('diagnostic')
-
-local mapper = function(mode, key, result)
-    vim.api.nvim_buf_set_keymap(0, mode, key, result, {noremap = true, silent = true})
-end
-
--- local setup_custom_diagnostics = function()
---     Diagnostic = require('vim.lsp.actions').Diagnostic
---     Location = require('vim.lsp.actions').Location
-
---     vim.lsp.callbacks["textDocument/publishDiagnostics"] = Diagnostic.handle_publish_diagnostics.with {
---         should_underline = false,
---         update_in_insert = false
---     }
-
---     mapper(
---         'n',
---         '<leader>dn',
---         '<cmd>lua vim.lsp.structures.Diagnostic.buf_move_next_diagnostic()<CR>'
---     )
-
---     mapper(
---         'n',
---         '<leader>dp',
---         '<cmd>lua vim.lsp.structures.Diagnostic.buf_move_prev_diagnostic()<CR>'
---     )
--- end
 
 -- Turn on status.
 lsp_status.register_progress()
@@ -35,36 +10,67 @@ lsp_status.register_progress()
 local custom_attach = function(client)
     completion.on_attach(client)
     diagnostic.on_attach(client)
-    lsp_status    .on_attach(client)
+    lsp_status.on_attach(client)
 
-    -- if false then
-    --     pcall(setup_custom_diagnostics)
-    -- end
+    -- Go to definition
+    vimp.nnoremap({'silent'}, '<Leader>gd', function()
+        vim.lsp.buf.definition()
+    end)
 
-    -- mapper('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-    mapper('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>')
-    mapper('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-    mapper('n', '1gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-    mapper('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-    mapper('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-    mapper('n', '<space>cr', '<cmd>lua vim.lsp.buf.rename()<CR>')
+    -- Go to implementation
+    vimp.nnoremap({'silent'}, '<Leader>gi', function()
+        vim.lsp.buf.implementation()
+    end)
 
-    -- if not vim.api.nvim_buf_get_keymap(0, 'n')['K'] then
-    if vim.api.nvim_buf_get_option(0, 'filetype') ~= 'lua' then
-        mapper('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-    end
+    -- Go to type definition
+    vimp.nnoremap({'silent'}, '<Leader>gtd', function()
+        vim.lsp.buf.type_definition()
+    end)
 
-    mapper('n', '<space>sl', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-    mapper('n', '<space>gd', '<cmd>lua vim.lsp.buf.definition { callbacks = { Location.jump_first, Location.highlight.with { timeout = 300 } } }<CR>')
+    -- Go to references
+    vimp.nnoremap({'silent'}, '<Leader>gr', function()
+        vim.lsp.buf.references()
+    end)
 
-    mapper('n', '<space>pd', '<cmd>lua vim.lsp.buf.definition { callbacks = Location.preview.with { lines_below = 5 } }<CR>')
+    -- Code action
+    vimp.nnoremap({'silent'}, '<Leader>ca', function()
+        vim.lsp.buf.code_action()
+    end)
+
+    -- Rename
+    vimp.nnoremap({'silent'}, '<Leader>r', function()
+        vim.lsp.buf.rename()
+    end)
+
+    -- Hover
+    vimp.nnoremap({'silent'}, 'K', function()
+        vim.lsp.buf.hover()
+    end)
+
+    -- Show line diagnostics
+    vimp.nnoremap({'silent'}, '<Leader>-', function()
+        vim.lsp.util.show_line_diagnostics()
+    end)
+
+    -- Signature help
+    vimp.nnoremap({'silent'}, '<Leader>sh', function()
+        vim.lsp.buf.signature_help()
+    end)
+
+    -- Go to next diagnostic
+    vimp.nnoremap({'silent'}, ']d', function()
+        vim.lsp.structures.Diagnostic.buf_move_next_diagnostic()
+    end)
+
+    -- Go to previous diagnostic
+    vimp.nnoremap({'silent'}, '[d', function()
+        vim.lsp.structures.Diagnostic.buf_move_prev_diagnostic()
+    end)
 
     -- Rust is currently the only thing w/ inlay hints
     if vim.api.nvim_buf_get_option(0, 'filetype') == 'rust' then
         vim.cmd [[autocmd BufEnter,BufWritePost <buffer> :lua require('lsp_extensions.inlay_hints').request { aligned = true, prefix = " » " }]]
     end
-
-    mapper('i', '<c-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
 
     vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
 end
