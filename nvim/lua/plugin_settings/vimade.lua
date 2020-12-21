b4d3950c3a8563cd7e49ca1937eb352b2b6552fa
Luke Xavier Symington <lxsymington@gmail.com>
2020-12-21 17:56:17 +0000

    Further faff with efm

--- nvim/lua/plugin_settings/vimade.lua
+++ nvim/lua/plugin_settings/vimade.lua
@@ -1,6 +1,11 @@
 local M = {}
 
 -- VIMADE ------------------------------
-function M.setup() vim.g.vimade = {fadelevel = 0.6, enablesigns = 1} end
+function M.setup() vim.g.vimade = {
+    fadelevel = 0.6,
+    enablesigns = 1,
+    enabletreesitter = 1,
+    groupscrollbind = 1
+} end
 
 return M
