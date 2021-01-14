6c7572046f12c1e83f999d57952e940958f35127
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-01-14 17:40:05 +0000

    Eslint fix wipes files atm

--- nvim/lua/plugin_settings/efm/eslint.lua
+++ nvim/lua/plugin_settings/efm/eslint.lua
@@ -1,6 +1,6 @@
 return {
-    formatCommand = "./node_modules/.bin/eslint --fix-dry-run -f json --stdin --stdin-filename ${INPUT}",
-    formatStdin = true,
+    -- formatCommand = "./node_modules/.bin/eslint --fix-dry-run -f json --stdin --stdin-filename ${INPUT}",
+    -- formatStdin = true,
     lintCommand = "./node_modules/.bin/eslint -f unix --stdin --stdin-filename ${INPUT}",
     lintIgnoreExitCode = true,
     lintStdin = true
