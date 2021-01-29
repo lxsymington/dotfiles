708d10394599ba39d076a338b5c3701678a7e0ea
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-01-29 10:46:49 +0000

    eslint formats

--- nvim/lua/plugin_settings/efm/eslint.lua
+++ nvim/lua/plugin_settings/efm/eslint.lua
@@ -1,7 +1,8 @@
 return {
-    formatCommand = "eslint_d --fix-to-stdout -f unix --stdin --stdin-filename ${INPUT}",
+    formatCommand = "eslint_d --cache --fix-to-stdout -f unix --stdin --stdin-filename ${INPUT}",
     formatStdin = true,
-    lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+    lintCommand = "eslint_d --cache -f unix --stdin --stdin-filename ${INPUT}",
     lintIgnoreExitCode = true,
-    lintStdin = true
+    lintStdin = true,
+    lintFormats = {'%f:%l:%c: %m'}
 }
