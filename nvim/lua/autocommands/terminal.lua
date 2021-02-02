18591572a66582658b6076c7ecd4d479e4a369de
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-02-02 17:25:45 +0000

    Terminal options

--- nvim/lua/autocommands/terminal.lua
+++ nvim/lua/autocommands/terminal.lua
@@ -1,25 +1,30 @@
 -- TERMINAL
 local M = {}
 
-if not _G.autocommand then _G.autocommand = {} end
-
-function _G.autocommand.terminal()
-    -- Disables line numbers
-    vim.wo.number = false
-    vim.wo.relativenumber = false
-
-    -- Remove the signcolumn
-    vim.wo.signcolumn = 'no'
-
-    -- Remove the foldcolumn
-    vim.wo.foldcolumn = '0'
-end
-
 function M.setup()
+    if not _G.autocommand then _G.autocommand = {} end
+
+    function _G.autocommand.terminal()
+        -- Disables line numbers
+        vim.o.number = false
+        vim.wo.number = false
+        vim.o.relativenumber = false
+        vim.wo.relativenumber = false
+
+        -- Remove the signcolumn
+        vim.o.signcolumn = 'no'
+        vim.wo.signcolumn = 'no'
+
+        -- Remove the foldcolumn
+        vim.o.foldcolumn = '0'
+        vim.wo.foldcolumn = '0'
+    end
+
     vim.api.nvim_command([[
         augroup Terminal
         autocmd! * <buffer>
-        autocmd TermOpen * v:lua.autocommand.terminal() 
+        autocmd TermOpen * setlocal statusline=%{b:term_title}
+        autocmd TermOpen * lua autocommand.terminal() 
         augroup END
     ]])
 end
