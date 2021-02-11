24cbca2e8dcda45d8feefc4d763ac27cff65f892
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-02-11 14:23:09 +0000

    Snippets fix

--- nvim/lua/plugin_settings/snippets.lua
+++ nvim/lua/plugin_settings/snippets.lua
@@ -1,12 +1,12 @@
-local snippets = reuire('snippets')
+local snippets = require('snippets')
 local M = {}
 
 -- SNIPPETS ----------------------------
 function M.setup()
     snippets.use_suggested_mappings()
 
-    vimp.inoremap({'silent'}, '<c-k>', function() snippets.expand_or_advance(1) end)
-    vimp.inoremap({'silent'}, '<c-j>', function() snippets.advance_snippet(-1) end)
+    vimp.inoremap({'silent'}, '<Leader>p', function() snippets.expand_or_advance(1) end)
+    vimp.inoremap({'silent'}, '<Leader>n', function() snippets.advance_snippet(-1) end)
 
     snippets.snippets = {}
 end
