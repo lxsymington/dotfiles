555e3f11a4c3b805f6e33cbb3dc9b537e7da6600
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-05-10 16:47:27 +0100

    Lazy-load most packages

--- nvim/lua/lxs/dependencies.lua
+++ nvim/lua/lxs/dependencies.lua
@@ -6,8 +6,11 @@ if not packer_exists then
 
 	vim.fn.mkdir(directory, "p")
 
-	local out =
-		vim.fn.system(string.format("git clone %s %s", "https://github.com/wbthomason/packer.nvim", directory .. "packer.nvim"))
+	local out = vim.fn.system(string.format(
+		"git clone %s %s",
+		"https://github.com/wbthomason/packer.nvim",
+		directory .. "packer.nvim"
+	))
 
 	print(out)
 	print("Downloading packer.nvim")
@@ -25,255 +28,309 @@ vim.api.nvim_exec(
 	false
 )
 
-return require("packer").startup({
-	function(use)
-		-- Packer can manage itself as an optional plugin
-		use({ "wbthomason/packer.nvim", opt = true })
+return require("packer").startup(function(use)
+	-- Packer can manage itself as an optional plugin
+	use({ "wbthomason/packer.nvim", opt = true })
 
-		-- Config
-		use("tjdevries/astronauta.nvim")
+	-- Config
+	use("tjdevries/astronauta.nvim")
 
-		-- Utilities
-		use("nvim-lua/popup.nvim")
-		use({ "nvim-lua/plenary.nvim" })
+	-- Utilities
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
 
-		-- Highlighting
-		use({
-			"nvim-treesitter/nvim-treesitter",
-			requires = { "nvim-treesitter/nvim-treesitter-textobjects" },
-			config = function()
-				require("lxs.plugin_settings.treesitter")
-			end,
-			run = function()
-				vim.cmd("TSUpdate")
-			end,
-		})
+	-- Highlighting
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		requires = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		config = function()
+			require("lxs.plugin_settings.treesitter")
+		end,
+		run = function()
+			vim.cmd("TSUpdate")
+		end,
+	})
 
-		-- File Explorer
-		use({
-			"kyazdani42/nvim-tree.lua",
-			requires = { "kyazdani42/nvim-web-devicons" },
-			config = function()
-				require("lxs.plugin_settings.nvim_tree").setup()
-			end,
-		})
+	use({ "kyazdani42/nvim-web-devicons" })
 
-		-- Snippets
-		use({
-			"norcalli/snippets.nvim",
-			config = function()
-				require("lxs.plugin_settings.snippets").setup()
-			end,
-		})
+	-- File Explorer
+	use({
+		"kyazdani42/nvim-tree.lua",
+		after = "nvim-web-devicons",
+		config = function()
+			require("lxs.plugin_settings.nvim_tree").setup()
+		end,
+		opt = true,
+		cmd = {
+			"NvimTreeClipboard",
+			"NvimTreeClose",
+			"NvimTreeFindFile",
+			"NvimTreeOpen",
+			"NvimTreeRefresh",
+			"NvimTreeToggle",
+		},
+		keys = { "n", "<Leader><Tab>" },
+	})
 
-		-- Neovim Lua Helper
-		use("tjdevries/nlua.nvim")
-		use("euclidianAce/BetterLua.vim")
-		use("bfredl/nvim-luadev")
+	-- Snippets
+	use({
+		"norcalli/snippets.nvim",
+		config = function()
+			require("lxs.plugin_settings.snippets").setup()
+		end,
+	})
 
-		-- Completion
-		use({
-			"hrsh7th/nvim-compe",
-			config = function()
-				require("lxs.plugin_settings.compe").setup()
-			end,
-		})
+	-- Neovim Lua Helper
+	use("tjdevries/nlua.nvim")
+	use("euclidianAce/BetterLua.vim")
+	use("bfredl/nvim-luadev")
 
-		-- LSP configurations
-		use({
-			"neovim/nvim-lspconfig",
-			requires = {
-				"nvim-lua/lsp-status.nvim",
-				"nvim-lua/lsp_extensions.nvim",
-			},
-			config = function()
-				require("lxs.lsp_config").setup()
-			end,
-		})
+	-- Completion
+	use({
+		"hrsh7th/nvim-compe",
+		config = function()
+			require("lxs.plugin_settings.compe").setup()
+		end,
+	})
 
-		-- LSP workspace diagnostics
-		use({
-			"folke/lsp-trouble.nvim",
-			requires = "kyazdani42/nvim-web-devicons",
-			config = function()
-				require("lxs.plugin_settings.lsp_trouble").setup()
-			end,
-		})
+	-- LSP configurations
+	use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			"nvim-lua/lsp-status.nvim",
+			"nvim-lua/lsp_extensions.nvim",
+		},
+		config = function()
+			require("lxs.lsp_config").setup()
+		end,
+	})
 
-		-- LSP file outline
-		use({
-			"simrat39/symbols-outline.nvim",
-			config = function()
-				require("lxs.plugin_settings.symbols_outline").setup()
-			end,
-		})
+	-- LSP workspace diagnostics
+	use({
+		"folke/lsp-trouble.nvim",
+		after = "nvim-web-devicons",
+		config = function()
+			require("lxs.plugin_settings.lsp_trouble").setup()
+		end,
+		opt = true,
+		cmd = {
+			"LspTrouble",
+			"LspTroubleClose",
+			"LspTroubleDocumentOpen",
+			"LspTroubleDocumentToggle",
+			"LspTroubleOpen",
+			"LspTroubleRefresh",
+			"LspTroubleToggle",
+			"LspTroubleWorkspaceOpen",
+			"LspTroubleWorkspaceToggle",
+		},
+		keys = { "n", "<Leader>lt" },
+	})
 
-		-- Highlight colours
-		use({
-			"norcalli/nvim-colorizer.lua",
-			config = function()
-				require("colorizer").setup({
-					"*", -- Highlight all files, but customize some others.
-					css = { css = true }, -- Enable parsing rgb(...) functions in css.
-					html = { names = false }, -- Disable parsing "names" like Blue or Gray
-				})
-			end,
-		})
+	-- LSP file outline
+	use({
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("lxs.plugin_settings.symbols_outline").setup()
+		end,
+		opt = true,
+		cmd = {
+			"SymbolsOutline",
+			"SymbolsOutlineOpen",
+			"SymbolsOutlineClose",
+		},
+		keys = { "n", "<Leader>so" },
+	})
 
-		-- Zen mode
-        use({
-            "kdav5758/TrueZen.nvim",
-            config = function()
-                require("lxs.plugin_settings.true_zen").setup()
-            end
-        })
-        
-        -- Indent markers
-        use {
-            'lukas-reineke/indent-blankline.nvim',
-            branch="lua",
-            config = function()
-                require("lxs.plugin_settings.indent_blankline").setup()
-            end
-        }
+	-- Highlight colours
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				"*", -- Highlight all files, but customize some others.
+				css = { css = true }, -- Enable parsing rgb(...) functions in css.
+				html = { names = false }, -- Disable parsing "names" like Blue or Gray
+			})
+		end,
+	})
 
-		-- Search
-		use({
-			"nvim-telescope/telescope.nvim",
-			requires = {
-				"nvim-lua/popup.nvim",
-				"nvim-lua/plenary.nvim",
-				"tami5/sql.nvim",
-				"nvim-telescope/telescope-frecency.nvim",
-				"nvim-telescope/telescope-fzy-native.nvim",
-			},
-			config = function()
-				require("lxs.plugin_settings.telescope").setup()
-			end,
-		})
+	-- Zen mode
+	use({
+		"kdav5758/TrueZen.nvim",
+		config = function()
+			require("lxs.plugin_settings.true_zen").setup()
+		end,
+	})
 
-		-- Version Control
-		use({
-			"TimUntersberger/neogit",
-			config = function()
-				require("lxs.plugin_settings.neogit").setup()
-			end,
-		})
-		use({
-			"tpope/vim-fugitive",
-			config = function()
-				require("lxs.plugin_settings.fugitive").setup()
-			end,
-		})
-		use({
-			"lewis6991/gitsigns.nvim",
-			requires = { "nvim-lua/plenary.nvim" },
-			config = function()
-				require("lxs.plugin_settings.gitsigns").setup()
-			end,
-		})
+	-- Indent markers
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		branch = "lua",
+		config = function()
+			require("lxs.plugin_settings.indent_blankline").setup()
+		end,
+	})
 
-		-- Statusline
-		use({
-			"famiu/feline.nvim",
-			requires = {
-				"kyazdani42/nvim-web-devicons",
-				"lewis6991/gitsigns.nvim",
-				"neovim/nvim-lspconfig",
-			},
-			config = function()
-				require("lxs.plugin_settings.feline").setup()
-			end,
-		})
+	-- Search
+	use("tami5/sql.nvim")
+    use("nvim-telescope/telescope-frecency.nvim")
+    use("nvim-telescope/telescope-fzy-native.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		after = {
+			"popup.nvim",
+			"plenary.nvim",
+			"sql.nvim",
+            "telescope-frecency.nvim",
+            "telescope-fzy-native.nvim",
+		},
+		config = function()
+			require("lxs.plugin_settings.telescope").setup()
+		end,
+		opt = true,
+		cmd = { "Telescope" },
+		keys = {
+			{ "n", "<Leader>ff" },
+			{ "n", "<Leader>fgf" },
+			{ "n", "<Leader>fb" },
+			{ "n", "<Leader>fd" },
+			{ "n", "<Leader>fht" },
+			{ "n", "<Leader>lg" },
+			{ "n", "<Leader>lbg" },
+			{ "n", "<Leader>sr" },
+			{ "n", "<Leader><space>" },
+			{ "n", "<Leader>?" },
+			{ "n", "<Leader>gb" },
+			{ "n", "<Leader>gs" },
+		},
+	})
 
-		-- Landing page + sessions
-		use({
-			"mhinz/vim-startify",
-			config = function()
-				require("lxs.plugin_settings.startify").setup()
-			end,
-		})
+	-- Version Control
+	use({
+		"TimUntersberger/neogit",
+		config = function()
+			require("lxs.plugin_settings.neogit").setup()
+		end,
+		opt = true,
+		cmd = { 'Neogit' },
+		keys = { 'n', '<Leader>g' }
+	})
+	use({
+		"tpope/vim-fugitive",
+		config = function()
+			require("lxs.plugin_settings.fugitive").setup()
+		end,
+	})
+	use({
+		"lewis6991/gitsigns.nvim",
+		after = { "plenary.nvim" },
+		config = function()
+			require("lxs.plugin_settings.gitsigns").setup()
+		end,
+	})
 
-		-- Debugger
-		use({
-			"mfussenegger/nvim-dap",
-			requires = { "theHamsta/nvim-dap-virtual-text" },
-			config = function()
-				require("lxs.plugin_settings.nvim_dap").setup()
-			end,
-		})
-		use({
-			"nvim-telescope/telescope-dap.nvim",
-			after = { "telescope.nvim", "nvim-dap" },
-			config = function()
-				require("lxs.plugin_settings.nvim_dap.telescope_integration").setup()
-			end,
-		})
+	-- Statusline
+	use({
+		"famiu/feline.nvim",
+		after = {
+			"nvim-web-devicons",
+			"gitsigns.nvim",
+			"nvim-lspconfig",
+		},
+		config = function()
+			require("lxs.plugin_settings.feline").setup()
+		end,
+	})
 
-		-- Markdown Preview
-		use({
-			"euclio/vim-markdown-composer",
-			run = "cargo build --release",
-			opt = true,
-			ft = { "md" },
-		})
+	-- Landing page + sessions
+	use({
+		"mhinz/vim-startify",
+		config = function()
+			require("lxs.plugin_settings.startify").setup()
+		end,
+	})
 
-		-- Databases
-		use({
-			"kristijanhusak/vim-dadbod-ui",
-			requires = { "tpope/vim-dadbod" },
-			opt = true,
-			cmd = {
-				"DB",
-				"DBUI",
-				"DBUIAddConnection",
-				"DBUIFindBuffer",
-				"DBUILastQueryInfo",
-				"DBUIRenameBuffer",
-				"DBUIToggle",
-			},
-		})
+	-- Debugger
+	use({
+		"mfussenegger/nvim-dap",
+		requires = { "theHamsta/nvim-dap-virtual-text" },
+		config = function()
+			require("lxs.plugin_settings.nvim_dap").setup()
+		end,
+	})
+	use({
+		"nvim-telescope/telescope-dap.nvim",
+		after = { "telescope.nvim", "nvim-dap" },
+		config = function()
+			require("lxs.plugin_settings.nvim_dap.telescope_integration").setup()
+		end,
+	})
 
-		-- Github
-		use({
-			"pwntester/octo.nvim",
-			after = { "telescope.nvim" },
-			opt = true,
-			cmd = { "Octo" },
-		})
+	-- Markdown Preview
+	use({
+		"euclio/vim-markdown-composer",
+		run = "cargo build --release",
+	})
 
-		-- Colors
-		use({
-			"amadeus/vim-convert-color-to",
-			opt = true,
-			cmd = { "ConvertColorTo" },
-		})
+	-- Databases
+	use({
+		"kristijanhusak/vim-dadbod-ui",
+		requires = { "tpope/vim-dadbod" },
+		opt = true,
+		cmd = {
+			"DB",
+			"DBUI",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+			"DBUILastQueryInfo",
+			"DBUIRenameBuffer",
+			"DBUIToggle",
+		},
+	})
 
-		-- Colorscheme builder
-		use("rktjmp/lush.nvim")
+	-- Github
+	use({
+		"pwntester/octo.nvim",
+		after = { "telescope.nvim" },
+		opt = true,
+		cmd = { "Octo" },
+	})
 
-		-- Colorschemes
-		use({ "junegunn/seoul256.vim", opt = true })
+	-- Colors
+	use({
+		"amadeus/vim-convert-color-to",
+		opt = true,
+		cmd = { "ConvertColorTo" },
+	})
 
-		-- Usability
-		use("tpope/vim-abolish")
+	-- Colorscheme builder
+	use("rktjmp/lush.nvim")
 
-		-- Tmux
-		use("christoomey/vim-tmux-navigator")
+	-- Colorschemes
+	use({ "junegunn/seoul256.vim", opt = true })
 
-		-- Editor config
-		use("editorconfig/editorconfig-vim")
+	-- Usability
+	use("tpope/vim-abolish")
 
-		-- Surround
-		use("machakann/vim-sandwich")
+	-- Tmux
+	use("christoomey/vim-tmux-navigator")
 
-		-- Pairing
-		use("tmsvg/pear-tree")
+	-- Editor config
+	use("editorconfig/editorconfig-vim")
 
-		-- Commenting
-		use("b3nj5m1n/kommentary")
+	-- Surround
+	use("machakann/vim-sandwich")
 
-		-- Git Author
-		use("f-person/git-blame.nvim")
-	end,
+	-- Pairing
+	use("tmsvg/pear-tree")
+
+	-- Commenting
+	use("b3nj5m1n/kommentary")
+
+	-- Git Author
+	use("f-person/git-blame.nvim")
+end, {
+	display = {
+		open_fn = require("packer.util").float,
+	},
 })
