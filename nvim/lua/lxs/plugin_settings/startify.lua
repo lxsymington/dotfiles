4c9c688efa9c5b0d0be2c6581fd4808f90052907
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-07-23 12:46:24 +0100

    Startify session order

--- nvim/lua/lxs/plugin_settings/startify.lua
+++ nvim/lua/lxs/plugin_settings/startify.lua
@@ -5,8 +5,8 @@ function M.setup()
 	-- Automatically update sessions
 	vim.g.startify_session_persistence = 1
 
-	-- Sort sessions by modification time
-	vim.g.startify_session_sort = 1
+	-- Sort sessions alphabetically rather than by modification time
+	vim.g.startify_session_sort = 0
 
 	-- Set the session directory
 	vim.g.startify_session_dir = "~/.local/share/nvim/sessions"
