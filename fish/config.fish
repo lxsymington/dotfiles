3c086c90a52171f11b92f734f760f463a6359017
Luke Xavier Symington <lukexaviersymington@gmail.com>
2021-11-09 21:38:28 +0000

    WSL setup

--- fish/config.fish
+++ fish/config.fish
@@ -15,7 +15,7 @@ end
 
 # Launch keychain on login
 if type -q keychain && status is-interactive
-    keychain --eval --quiet -Q --inherit local --agents ssh id_rsa | source
+    keychain --eval --quiet -Q --inherit local --agents ssh id_ed25519 | source
 
     # Source relevant keychain files
     begin
