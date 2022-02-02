b95a4c652522d018cf4df6c4d4f393663361c7e1
Luke Xavier Symington <lukexaviersymington@gmail.com>
2022-02-02 12:22:56 +0000

    FNM config and tmux shortcut

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,11 +1,13 @@
 if type -q fnm
     fnm env --use-on-cd | source
+    set -l FNM_BIN $FNM_DIR/node-versions/(fnm current)/installation/bin
+    set -l FNM_LIB $FNM_DIR/node-versions/(fnm current)/installation/lib
 
-    if test -n "$NVM_BIN"
-        set -Ux NVM_BIN $FNM_DIR/node-versions/(fnm current)/installation/bin
+    if test -n "$NVM_BIN" -o "$NVM_BIN" != "$FNM_BIN"
+        set -Ux NVM_BIN $FNM_BIN
     end
 
-    if test -n "$NPM_GLOBAL_PACKAGE_PATH"
-        set -Ux NVM_BIN $FNM_DIR/node-versions/(fnm current)/installation/lib
+    if test -n "$NPM_GLOBAL_PACKAGE_PATH" -o "$NPM_GLOBAL_PACKAGE_PATH" != "$NVM_BIN"
+        set -Ux NPM_GLOBAL_PACKAGE_PATH $FNM_LIB 
     end
 end
