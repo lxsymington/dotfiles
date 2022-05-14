74e47ff899c7af2cb36dd4554b977b8dfb741f52
Luke Xavier Symington <lukexaviersymington@gmail.com>
2022-05-14 13:36:49 +0100

    Cross machine config

--- nvim/lua/lxs/diagnostics/formatter.lua
+++ nvim/lua/lxs/diagnostics/formatter.lua
@@ -5,81 +5,84 @@ local aucmd = vim.api.nvim_create_autocmd
 local M = {}
 
 local function select_config_file(config_files)
-	for _, config_file in ipairs(config_files) do
-		local fullPath = util.path.join(vim.loop.cwd, config_file)
+    for _, config_file in ipairs(config_files) do
+        local fullPath = util.path.join(vim.loop.cwd, config_file)
 
-		if util.path.is_file(fullPath) then
-			return fullPath
-		end
-	end
+        if util.path.is_file(fullPath) then
+            return fullPath
+        end
+    end
 end
 
 function M.prettier()
-	vim.loop.os_setenv('PRETTIERD_LOCAL_PRETTIER_ONLY', true)
+    if not vim.loop.os_getenv('PRETTIERD_LOCAL_PRETTIER_ONLY') then
+        vim.loop.os_setenv('PRETTIERD_LOCAL_PRETTIER_ONLY', true)
+    end
 
-	if lsp_loaded then
-		local config_files = {
-			'.prettierrc',
-			'.prettierrc.json',
-			'.prettierrc.json5',
-			'.prettierrc.yml',
-			'.prettierrc.yaml',
-			'.prettierrc.js',
-			'.prettierrc.cjs',
-			'.prettierrc.config.js',
-			'.prettierrc.config.cjs',
-			'.prettierrc.toml',
-		}
+    local config_files = {
+        '.prettierrc',
+        '.prettierrc.json',
+        '.prettierrc.json5',
+        '.prettierrc.yml',
+        '.prettierrc.yaml',
+        '.prettierrc.js',
+        '.prettierrc.cjs',
+        '.prettierrc.config.js',
+        '.prettierrc.config.cjs',
+        '.prettierrc.toml',
+    }
 
-		local config = select_config_file(config_files)
+    local config = select_config_file(config_files)
 
-		vim.loop.os_setenv('PRETTIERD_DEFAULT_CONFIG', config)
-	end
+    if not (lsp_loaded or config) then
+        return nil
+    end
 
-	return {
-		exe = 'prettierd',
-		args = { vim.api.nvim_buf_get_name(0) },
-		stdin = true,
-	}
+    vim.loop.os_setenv('PRETTIERD_DEFAULT_CONFIG', config)
+
+    return {
+        exe = 'prettierd',
+        args = { vim.api.nvim_buf_get_name(0) },
+        stdin = true,
+    }
 end
 
 function M.stylua()
-	local config = vim.fn.findfile('stylua.toml', vim.loop.cwd() .. '/**')
-	local config_path = vim.fn.fnamemodify(config, ':p:.')
-	vim.pretty_print(config_path)
+    local config = vim.fn.findfile('stylua.toml', vim.loop.cwd() .. '/**')
+    local config_path = vim.fn.fnamemodify(config, ':p:.')
 
-	return {
-		exe = 'stylua',
-		args = {
-			'--config-path ' .. config_path,
-			'-',
-		},
-		stdin = true,
-	}
+    return {
+        exe = 'stylua',
+        args = {
+            '--config-path ' .. config_path,
+            '-',
+        },
+        stdin = true,
+    }
 end
 
 function M.setup()
-	formatter.setup({
-		filetype = {
-			javascript = {
-				M.prettier,
-			},
-			json = {
-				M.prettier,
-			},
-			lua = {
-				M.stylua,
-			},
-		},
-	})
+    formatter.setup({
+        filetype = {
+            javascript = {
+                M.prettier,
+            },
+            json = {
+                M.prettier,
+            },
+            lua = {
+                M.stylua,
+            },
+        },
+    })
 
-	local format_group = augrp('Format', { clear = true })
-	aucmd('BufWritePost', {
-		pattern = { '*.js', '*.cjs', '*.mjs', '*.jsx', '*.ts', '*.tsx', '*.lua' },
-		command = 'FormatWrite',
-		once = true,
-		group = format_group,
-	})
+    local format_group = augrp('Format', { clear = true })
+    aucmd('BufWritePost', {
+        pattern = { '*.js', '*.cjs', '*.mjs', '*.jsx', '*.ts', '*.tsx', '*.lua' },
+        command = 'FormatWrite',
+        once = true,
+        group = format_group,
+    })
 end
 
 return M
