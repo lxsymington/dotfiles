5f8ad3a52ca6b3486ac59ca11cd7bad248908e0d
Luke Xavier Symington <lukexaviersymington@gmail.com>
2022-07-21 22:56:11 +0100

    Small progress

--- plugins/lxsymington/conscript/lua/conscript/init.lua
+++ plugins/lxsymington/conscript/lua/conscript/init.lua
@@ -1,4 +1,5 @@
 local Job = require('plenary.job')
+local expand = vim.fn.expand
 local filereadable = vim.fn.filereadable
 local json = vim.json
 local M = {}
@@ -11,76 +12,78 @@ local M = {}
 ]]
 
 M.regiments = {
-	'*.ts',
-	'*.cts',
-	'*.mts',
-	'*.tsx',
-	'*.js',
-	'*.cjs',
-	'*.mjs',
-	'*.jsx',
-	'*.json',
-	'*.json5',
-	'*.yaml',
-	'*.yml',
+    '*.ts',
+    '*.cts',
+    '*.mts',
+    '*.tsx',
+    '*.js',
+    '*.cjs',
+    '*.mjs',
+    '*.jsx',
+    '*.json',
+    '*.json5',
+    '*.yaml',
+    '*.yml',
 }
 
 --  TODO: make it so that this only stores one set of scripts and can be easily cleared
 M.cache = {}
 
 function M.scout()
-	local is_node_project = filereadable('package.json')
+    local conscript_augroup = vim.api.nvim_create_augroup('ConscriptScout', { clear = true })
 
-	if not is_node_project then
-		vim.api.nvim_clear_autocmds({ group = 'ConscriptScout' })
-		return
-	end
+    local expected_path = expand(vim.loop.cwd() .. '/package.json')
+    local is_node_project = filereadable(expected_path) == 1
 
-	vim.notify('Node project detected, Enrolling conscript 💂', vim.log.levels.INFO)
+    if not is_node_project then
+        vim.api.nvim_clear_autocmds({ group = conscript_augroup })
+        return
+    end
 
-	local conscript_augroup = vim.api.nvim_create_augroup('ConscriptScout', { clear = true })
+    vim.notify('Node project detected, Enrolling conscript 💂', vim.log.levels.INFO)
 
-	vim.api.nvim_create_autocmd('BufEnter', {
-		pattern = M.regiments,
-		callback = M.patrol,
-		group = conscript_augroup,
-	})
+    vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = M.regiments,
+        callback = M.patrol,
+        group = conscript_augroup,
+        once = true
+    })
 
-	local package_json_path = vim.loop.cwd() .. '/package.json'
+    local package_json_path = vim.loop.cwd() .. '/package.json'
 
-	local package_json_handle = io.open(package_json_path, 'r')
+    local package_json_handle = io.open(package_json_path, 'r')
 
-	if not package_json_handle then
-		return
-	end
+    if not package_json_handle then
+        return
+    end
 
-	local package_json_contents = package_json_handle:read('*a')
-	package_json_handle:close()
-	local package_json = json.decode(package_json_contents)
+    local package_json_contents = package_json_handle:read('*a')
+    package_json_handle:close()
+    local package_json = json.decode(package_json_contents)
 
-	if not #package_json.scripts then
-		vim.notify('No scripts found', vim.log.levels.INFO)
-		return
-	end
+    if not #package_json.scripts then
+        vim.notify('No scripts found', vim.log.levels.INFO)
+        return
+    end
 
-	-- TODO: improve this
-	M.cache = vim.tbl_extend(M.cache, package_json.scripts)
+    -- TODO: improve this
+    M.cache = vim.tbl_extend('force', M.cache, package_json.scripts)
 end
 
 function M.exercises()
-	local script_names = vim.tbl_keys(M.cache)
+    local script_names = vim.tbl_keys(M.cache)
 
-	vim.ui.select(script_names, {
-		prompt = 'Select a script to run',
-		format_item = function(item)
-			return string.format('%s | %s', item, M.cache[item])
-		end,
-	}, M.drill)
+    vim.ui.select(script_names, {
+        prompt = 'Select a script to run',
+        format_item = function(item)
+            return string.format('%s | %s', item, M.cache[item])
+        end,
+    }, M.drill)
 end
 
 function M.patrol(...)
-	vim.pretty_print(...)
-	--[[ {
+    vim.pretty_print(...)
+    --[[ {
         buf = 7,
         event = "BufEnter",
         file = "/home/lxs/Development/secclint/src/filter.ts",
@@ -89,21 +92,21 @@ function M.patrol(...)
         match = "/home/lxs/Development/secclint/src/filter.ts"
     } ]]
 
-	-- vim.api.nvim_buf_get_commands(buffer: number, opts: table<string, any>)
-	-- vim.api.nvim_buf_create_user_command('Make',)
+    -- vim.api.nvim_buf_get_commands(buffer: number, opts: table<string, any>)
+    -- vim.api.nvim_buf_create_user_command('Make',)
 end
 
 function M.drill(script, idx)
-	Job
-		:new({
-			command = 'npm',
-			args = { 'run', script },
-			cwd = vim.loop.cwd(),
-			on_exit = function(...)
-				vim.pretty_print(...)
-			end,
-		})
-		:start()
+    Job
+        :new({
+            command = 'npm',
+            args = { 'run', script },
+            cwd = vim.loop.cwd(),
+            on_exit = function(...)
+                vim.pretty_print(...)
+            end,
+        })
+        :start()
 end
 
 return M
