0891de288ab24ef43a65226fe45b1c18132ae3d1
Luke Xavier Symington <lukexaviersymington@gmail.com>
2022-08-01 22:20:26 +0100

    More ansible config

--- nvim/lua/lxs/config/external.lua
+++ nvim/lua/lxs/config/external.lua
@@ -6,6 +6,16 @@ function M.setup()
 	-- Use ripgrep instead of grep
 	opt.grepprg = [[rg --vimgrep --no-heading --smart-case]]
 	opt.grepformat = [[%f:%l:%c:%m,%f:%l:%m]]
+
+	-- Set the compiler by filetype
+	local compiler_augroup = vim.api.nvim_create_augroup('Compiler', {})
+	vim.api.nvim_create_autocmd('BufRead', {
+	    callback = function ()
+            pcall(vim.cmd.execute, string.format('compiler %s', vim.bo.filetype))
+        end,
+        desc = 'Configure compiler for filetype',
+        group = compiler_augroup
+    })
 end
 
 return M
